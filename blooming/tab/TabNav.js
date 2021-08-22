b3a7e2fb24032aa5d193a5002662e63b35e97595
byhhh2 <rlaalsdl111@gmail.com>
2021-08-22 19:04:04 +0900

    initial commit

---
+++ blooming/tab/TabNav.js
@@ -0,0 +1,45 @@
+import React, {useEffect} from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Text, View, StyleSheet, BackHandler, Alert} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+import AddDiary from './add_diary/AddDiary';
+import Home from './home/Home';
+import Profile from './profile/Profile';
+import SharedDiary from './shared_diary/SharedDiary';
+
+const Tab = createBottomTabNavigator();
+
+const TabNav = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="SharedDiary" component={SharedDiary} />
+        <Tab.Screen name="AddDiary" component={AddDiary} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default TabNav;
