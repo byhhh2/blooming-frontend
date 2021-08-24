1f305c8143b1113530f883c1d1f268b0ca8a7842
byhhh2 <rlaalsdl111@gmail.com>
2021-08-24 21:11:14 +0900

    Add profile layout

--- blooming/tab/profile/ProfileStack.js
+++ blooming/tab/profile/ProfileStack.js
@@ -1,14 +1,29 @@
 import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 
 import Profile from './Profile';
+
+import Feather from 'react-native-vector-icons/Feather';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const Stack = createNativeStackNavigator();
 
 const ProfileStack = () => {
   return (
     <Stack.Navigator initialRouteName={Profile}>
-      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={({navigation}) => ({
+          title: '사용자',
+          headerTitleStyle: {
+            fontFamily: 'GmarketSansTTFMedium',
+            fontSize: 15,
+          },
+          headerTitleAlign: 'center',
+        })}
+      />
     </Stack.Navigator>
   );
 };
